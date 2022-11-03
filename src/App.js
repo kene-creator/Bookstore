0a4a19e4016c1cc5efed2a14051f6f90f34b0813
Kene <kayyungx@gmail.com>
2022-11-03 22:12:18 +0100

    Remove command disabling linter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/UI/Navbar.js';
--- src/components/UI/BookList.js
+++ src/components/UI/BookList.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-
 import Book from './Book.js';
 
 const BookList = (props) => (

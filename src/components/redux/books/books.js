c0bb7f9b25f134e5f370ddad4bc3825a9884fea5
Kene <kayyungx@gmail.com>
2022-11-08 12:31:02 +0100

    Add dummy book to state

--- src/components/redux/books/books.js
+++ src/components/redux/books/books.js
@@ -1,7 +1,40 @@
 const ADD_BOOK = 'book-store-app/books/ADD_BOOK';
 const REMOVE_BOOK = 'book-store-app/books/REMOVE_BOOK';
 
-const booksReducer = (state = [], action) => {
+const DUMMY_BOOKS = [
+  {
+    id: '1',
+    title: 'The Hunger games',
+    author: 'Suzanne Collins',
+    genre: 'Action',
+    percentage: 20
+  },
+
+  {
+    id: '2',
+    title: 'Rhapsody of Realities',
+    author: 'Pastor Chris',
+    genre: 'Action',
+    percentage: 45
+  },
+
+  {
+    id: '3',
+    title: 'Eat that Frog',
+    author: 'John Maxwell',
+    genre: 'Action',
+    percentage: 75
+  },
+  {
+    id: '4',
+    title: 'The Hunger games',
+    author: 'Suzanne Collins',
+    genre: 'Action',
+    percentage: 43
+  }
+];
+
+const booksReducer = (state = DUMMY_BOOKS, action) => {
   if (action.type === ADD_BOOK) {
     return [
       ...state,
